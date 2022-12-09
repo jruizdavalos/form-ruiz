f359dfaeb39fc8001fb388def4a31a3b39431d83
Juan Ruiz <jaruizdavalos@gmail.com>
2022-12-08 21:50:24 -0300

    Entregable de reactive-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MainFormComponent } from './main-form/main-form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainFormComponent,
+
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/main-form/main-form.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { __values } from 'tslib';
+import { debounceTime } from 'rxjs';
+
+@Component({
+  selector: 'app-main-form',
+  templateUrl: './main-form.component.html',
+  styleUrls: ['./main-form.component.css']
+})
+
+export class MainFormComponent implements OnInit {
+  nameControl = new FormControl('', [Validators.required])
+  lastNameControl = new FormControl('', [Validators.required])
+  dateControl = new FormControl('', [Validators.required])
+  genderControl = new FormControl('', [Validators.required])
+
+  emailControl = new FormControl('', [Validators.required])
+  textControl = new FormControl('')
+
+
+  registerForm = new FormGroup({
+    name: this.nameControl,
+    lastName: this.lastNameControl,
+    date: this.dateControl,
+    gender: this.genderControl,
+    email: this.emailControl,
+    text: this.textControl,
+  })
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  }
+  onSubmit() {
+    console.log(this.registerForm.value)
+  }
+
+}
